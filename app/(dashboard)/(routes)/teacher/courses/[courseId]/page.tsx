8bfdb66b0b69bdb6d7f437e0ec66dcdf60d47786
igorc <igorcastilhos2010@hotmail.com>
2024-06-21 17:11:39 -0300

    feat: Add course edit page UI

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
@@ -1,7 +1,53 @@
-const CourseIdPage = ({params}: { params: { courseId: string } }) => {
+import prisma from "@/lib/db";
+import {auth} from "@clerk/nextjs/server";
+import {redirect} from "next/navigation";
+import {IconBadge} from "@/components/icon-badge";
+import {LayoutDashboard} from "lucide-react";
+
+const CourseIdPage = async ({params}: { params: { courseId: string } }) => {
+    const {userId} = auth();
+    if (!userId) return redirect("/");
+
+    const course = await prisma.course.findUnique({where: {id: params.courseId}});
+    if (!course) return redirect("/");
+
+    const requiredFiels = [
+        course.title,
+        course.description,
+        course.imageUrl,
+        course.price,
+        course.categoryId
+    ];
+    // Gets the number of all the required fields
+    const totalFields = requiredFiels.length;
+    // Gets the number of all the completed fields (which don't equal to false)
+    const completedFields = requiredFiels.filter(Boolean).length;
+    const completionText = `(${completedFields}/${totalFields})`;
+
     return (
-        <div>
-            Course ID: {params.courseId}
+        <div className="p-6">
+            <div className="flex items-center justify-between">
+                <div className="flex flex-col gap-y-2">
+                    <h1 className="text-2xl font-medium">
+                        Setup do Curso
+                    </h1>
+                    <span className="text-sm text-slate-700">
+                        Complete todas as informações {completionText}
+                    </span>
+                </div>
+            </div>
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mt-16">
+                <div>
+                    <div className="flex items-center gap-x-2">
+                        <IconBadge icon={LayoutDashboard}/>
+                        <h2 className="text-xl">
+                            Customize o seu curso
+                        </h2>
+
+                    </div>
+                </div>
+
+            </div>
         </div>
     );
 }
