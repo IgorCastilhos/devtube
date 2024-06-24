e666bc8a7ffc8e2fc43ba106641656fdfaf37221
igorc <igorcastilhos2010@hotmail.com>
2024-06-24 02:55:22 -0300

    feat: Add course attachment form

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
@@ -2,17 +2,21 @@ import prisma from "@/lib/db";
 import {auth} from "@clerk/nextjs/server";
 import {redirect} from "next/navigation";
 import {IconBadge} from "@/components/icon-badge";
-import {CircleDollarSign, LayoutDashboard, ListChecks} from "lucide-react";
+import {CircleDollarSign, LayoutDashboard, ListChecks, File} from "lucide-react";
 import {TitleForm} from "@/app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/title-form";
 import {DescriptionForm} from "@/app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/description-form";
 import {ImageForm} from "@/app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/image-form";
 import {PriceForm} from "@/app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/price-form";
+import {AttachmentForm} from "@/app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/attachment-form";
 
 const CourseIdPage = async ({params}: { params: { courseId: string } }) => {
     const {userId} = auth();
     if (!userId) return redirect("/");
 
-    const course = await prisma.course.findUnique({where: {id: params.courseId}});
+    const course = await prisma.course.findUnique({
+        where: {id: params.courseId},
+        include: {attachments: {orderBy: {createdAt: "desc"}}}
+    });
     if (!course) return redirect("/");
 
     // const categories = await prisma.category.findMany({
@@ -54,18 +58,9 @@ const CourseIdPage = async ({params}: { params: { courseId: string } }) => {
                             Customize o seu curso
                         </h2>
                     </div>
-                    <TitleForm
-                        initialData={course}
-                        courseId={course.id}
-                    />
-                    <DescriptionForm
-                        initialData={course}
-                        courseId={course.id}
-                    />
-                    <ImageForm
-                        initialData={course}
-                        courseId={course.id}
-                    />
+                    <TitleForm initialData={course} courseId={course.id}/>
+                    <DescriptionForm initialData={course} courseId={course.id}/>
+                    <ImageForm initialData={course} courseId={course.id}/>
                     {/*<CategoryForm*/}
                     {/*    initialData={course}*/}
                     {/*    courseId={course.id}*/}
@@ -96,6 +91,15 @@ const CourseIdPage = async ({params}: { params: { courseId: string } }) => {
                         </div>
                         <PriceForm initialData={course} courseId={course.id}/>
                     </div>
+                    <div>
+                        <div className="flex items-center gap-x-2">
+                            <IconBadge icon={File}/>
+                            <h2 className="text-xl">
+                                Recursos & Anexos
+                            </h2>
+                        </div>
+                    </div>
+                    <AttachmentForm initialData={course} courseId={course.id}/>
                 </div>
             </div>
         </div>
