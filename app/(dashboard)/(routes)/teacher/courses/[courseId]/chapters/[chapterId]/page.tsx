e2e40ac90390a769d094a39712e1870db4957fe9
igorc <igorcastilhos2010@hotmail.com>
2024-06-25 01:25:50 -0300

    feat: Add Chapter Access Form

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/chapters/[chapterId]/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/chapters/[chapterId]/page.tsx
@@ -2,7 +2,7 @@ import {auth} from "@clerk/nextjs/server";
 import {redirect} from "next/navigation";
 import prisma from "@/lib/db";
 import Link from "next/link";
-import {ArrowLeft, LayoutDashboard} from "lucide-react";
+import {ArrowLeft, Eye, LayoutDashboard, Video} from "lucide-react";
 import {IconBadge} from "@/components/icon-badge";
 import {
     ChapterTitleForm
@@ -10,6 +10,9 @@ import {
 import {
     ChapterDescriptionForm
 } from "@/app/(dashboard)/(routes)/teacher/courses/[courseId]/chapters/[chapterId]/_components/chapter-description-form";
+import {
+    ChapterAccessForm
+} from "@/app/(dashboard)/(routes)/teacher/courses/[courseId]/chapters/[chapterId]/_components/chapter-access-form";
 
 const ChapterIdPage = async ({
                                  params
@@ -38,44 +41,69 @@ const ChapterIdPage = async ({
     const completionText = `(${completedFields}/${totalFields})`;
 
     return (
-        <div className="p-6">
-            <div className="flex items-center justify-between">
-                <div className="w-full">
-                    <Link href={`/teacher/courses/${params.courseId}`}
-                          className="flex items-center text-sm hover:opacity-75 transition mb-6">
-                        <ArrowLeft className="h-4 w-4 mr-2"/>
-                        Voltar para a configuração do curso
-                    </Link>
-                    <div className="flex items-center justify-between w-full">
-                        <div className="flex flex-col gap-y-2">
-                            <h1 className="text-2xl font-medium">
-                                Criação do Capítulo
-                            </h1>
-                            <span className="text-sm text-slate-700">
-                                Complete todas as informações {completionText}
-                            </span>
+        <>
+            <div className="p-6">
+                <div className="flex items-center justify-between">
+                    <div className="w-full">
+                        <Link
+                            href={`/teacher/courses/${params.courseId}`}
+                            className="flex items-center text-sm hover:opacity-75 transition mb-6"
+                        >
+                            <ArrowLeft className="h-4 w-4 mr-2"/>
+                            Voltar para a configuração do curso
+                        </Link>
+                        <div className="flex items-center justify-between w-full">
+                            <div className="flex flex-col gap-y-2">
+                                <h1 className="text-2xl font-medium">Criação do Capítulo</h1>
+                            </div>
                         </div>
+                        <span className="text-sm text-slate-700 dark:text-slate-300 ">
+                Complete todos os campos {completionText}
+            </span>
                     </div>
                 </div>
-            </div>
-            <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mt-16">
-                <div className="space-x-4">
-                    <div>
+                <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mt-16">
+                    <div className="space-y-4">
+                        <div>
+                            <div className="flex items-ceenter gap-x-2">
+                                <IconBadge icon={LayoutDashboard}/>
+                                <h2 className="text-xl font-medium">Personalize o capítulo</h2>
+                            </div>
+                            <ChapterTitleForm
+                                initialData={chapter}
+                                courseId={params.courseId}
+                                chapterId={params.chapterId}
+                            />
+                            <ChapterDescriptionForm
+                                initialData={chapter}
+                                courseId={params.courseId}
+                                chapterId={params.chapterId}
+                            />
+                        </div>
                         <div className="flex items-center gap-x-2">
-                            <IconBadge icon={LayoutDashboard}/>
-                            <h2 className="text-xl">
-                                Customize o seu capítulo
-                            </h2>
+                            <IconBadge icon={Eye}/>
+                            <h2 className="text-xl font-medium">Acessar configurações</h2>
                         </div>
-                        <ChapterTitleForm initialData={chapter} courseId={params.courseId}
-                                          chapterId={params.chapterId}/>
-                        <ChapterDescriptionForm initialData={chapter} courseId={params.courseId}
-                                                chapterId={params.chapterId}/>
+                        <ChapterAccessForm
+                            initialData={chapter}
+                            courseId={params.courseId}
+                            chapterId={params.chapterId}
+                        />
+                    </div>
+                    <div className="space-y-4">
+                        <div className="flex items-center gap-x-2">
+                            <IconBadge icon={Video}/>
+                            <h2 className="text-xl font-medium">Adicionar video</h2>
+                        </div>
+                        {/*<ChapterVideoForm*/}
+                        {/*    initialData={chapter}*/}
+                        {/*    courseId={params.courseId}*/}
+                        {/*    chapterId={params.chapterId}*/}
+                        {/*/>*/}
                     </div>
                 </div>
-
             </div>
-        </div>
+        </>
     );
 }
 
