6837cee3da15bd6aed072233ccaf471fc954cf02
igorc <igorcastilhos2010@hotmail.com>
2024-07-11 04:17:08 -0300

    update: updates the middleware file

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,6 @@
 import {clerkMiddleware, createRouteMatcher} from "@clerk/nextjs/server";
 
-const isPublicRoute = createRouteMatcher(["/api/webhook", "/api/uploadthing"]);
+const isPublicRoute = createRouteMatcher(["/api/webhook", "/api/uploadthing", "https://devtube.live", "https://devtube.live/sign-in", "https://devtube.live/sign-up"]);
 
 export default clerkMiddleware((auth, request) => {
     if (!isPublicRoute(request)) {
